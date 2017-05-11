2abf33f3b7b8b79968b62d8e2612a001c750b4b2
Nizar Abak-kali <nizar.abak-kali@smile.fr>
2017-05-11 11:10:35 +0200

    shell qui contient des commandes et des raccourcies

--- run.sh
+++ run.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env zsh
 
+LOCATION=~/Documents/test/testMpegts/
+
+
 launch_test(){
 	echo "cleaning ..."
 	make clean
+	cleandots
 	echo "build ...."
 	make
-	GST_DEBUG_DUMP_DOT_DIR=./dots/ GST_DEBUG=4 ./test /home/niaba/Documents 0
-
+	GST_DEBUG_DUMP_DOT_DIR=./dots/ GST_DEBUG=2,tsdemux:6 ./test /home/niaba/Documents 2
+    echo "dots 2 svg ..."
+    alldots2svg
 }
 
 dot2svg(){
-    dot -Tsvg $1 > pipeline.svg
+    IMAGE_NAME=pipeline.svg
+    dot -Tsvg $1 > $IMAGE_NAME
+    eog $IMAGE_NAME &
+
+}
+
+alldots2svg(){
+    LOCATION=~/Documents/test/testMpegts/
+    python $LOCATION/creates_graphs.py
 }
 
 cleandots(){
-    rm ./dots/*.dot
+    LOCATION=~/Documents/test/testMpegts/dots/
+    rm $LOCATION/*.dot
 }
